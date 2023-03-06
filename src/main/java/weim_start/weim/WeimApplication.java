715791efd186fe74812b8c512569047bfd247428
HastyDoodler <wjdxorfla100@daum.net>
2023-03-06 09:42:38 +0900

    first commit

---
+++ src/main/java/weim_start/weim/WeimApplication.java
@@ -0,0 +1,13 @@
+package weim_start.weim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/weim_start/weim/WeimApplicationTests.java
@@ -0,0 +1,13 @@
+package weim_start.weim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
